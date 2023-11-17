6ba1ec98492ea272df027daa52f7f0d2ed578901
lee_c <gitleelee@gmail.com>
2023-11-16 21:28:05 -0800

    Create functions, properties & methods.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,30 @@
+/* 
+Create a Factory Function & Add Properties
+Factory Function Pattern for Pets
+*/
+
 const statusButton = document.querySelector("button");
 const pets = document.querySelector(".all-pets");
+console.log(statusButton, pets); //LOG
+// createPet function
+const createPet = function (name, species) {
+  const pet = {
+    name: name,
+    species: species,
+    isTired: 5,
+    // sleep function
+    sleep: function () {
+      console.log(`${this.name} needs a nap. Zzz...`);
+      this.isTired = 1;
+    },
+    // play  function
+    play: function () {
+      if (this.isTired === 10) {
+        console.log("Too tired to play.");
+        this.sleep();
+      } else {
+        console.log(`Yay ${this.name} loves to play!`);
+        this.isTired += 1;
+      }
+    }
+  };
